a075a9a5c152ab97b143dc26405c2846ac3e7562
scottbelden <scottabelden@gmail.com>
2020-12-02 00:30:21 -0500

    day 2

---
+++ day_02.py
@@ -0,0 +1,29 @@
+from collections import Counter
+from utils import answer1, answer2, get_input
+
+valid_count = 0
+for line in get_input("day_02_input"):
+    char_range, char, password = line.split()
+    min_count = int(char_range.split("-")[0])
+    max_count = int(char_range.split("-")[1])
+    char = char[0]
+
+    if min_count <= Counter(password)[char] <= max_count:
+        valid_count += 1
+
+answer1(valid_count)
+
+valid_count = 0
+for line in get_input("day_02_input"):
+    char_indexes, char, password = line.split()
+    index_one = int(char_indexes.split("-")[0]) - 1
+    index_two = int(char_indexes.split("-")[1]) - 1
+    char = char[0]
+
+    if (
+        (password[index_one] == char and password[index_two] != char)
+        or (password[index_one] != char and password[index_two] == char)
+    ):
+        valid_count += 1
+
+answer2(valid_count)
